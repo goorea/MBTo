3f8d3c5c765c119864b5a01a6660dcd3ad463304
jhs851 <jhs851@naver.com>
2021-05-02 01:41:58 +0900

    [style] eslint 룰 추가 및 적용
    -  object-curly-spacing [‘error’, ‘always’]로 추가함
    	- import 할 때나 인라인 객체에서 중괄호에 띄어쓰기가 없으면 답답한 느낌
    - react/jsx-curly-brace-presence ['error', { props: 'never', children: 'never' }]로 추가함
        - 문자열 props가 큰 따옴표로 통일되기 원함

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    'react/jsx-curly-brace-presence': [
+      'error',
+      { props: 'never', children: 'never' },
+    ],
+  },
 };
