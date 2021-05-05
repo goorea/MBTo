e76394d0f6936efb43de32d0694bfbf3051931fb
jhs851 <jhs851@naver.com>
2021-05-05 23:08:21 +0900

    [feat] 테마 추가
    - 많이 쓰일 법한 색들만 일단 추가
    - 앱의 primary 색은 미정 추후에 추가

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+export default {
+  white: '#fff',
+  black: '#000',
+  gray: '#ddd',
+  gray100: '#eee',
+  gray200: '#898989',
+  google: '#dd4b39',
+  facebook: '#3b5999',
+  naver: '#1ec800',
+  kakao: '#fee500',
+  apple: '#000',
+};
